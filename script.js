7b6cb3f3732bfa6ad0ce9600ffa70ab230f84420
Rajendra Nitharwal <42902636+nitharwalraju@users.noreply.github.com>
2019-11-20 15:56:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+let appId = '1c7c41ecb489c84f9925ca4841c20249';
+let units = 'metric'; 
+let searchMethod; // q means searching as a string.
+
+function getSearchMethod(searchTerm) {
+    if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+        searchMethod = 'zip';
+    else 
+        searchMethod = 'q';
+}
+
+function searchWeather(searchTerm) {
+    getSearchMethod(searchTerm);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+        .then((result) => {
+            return result.json();
+        }).then((res) => {
+            init(res);
+    });
+}
+
+window.onload = function() {
+    icon = document.getElementById("icon");
+    temp = document.getElementById("temp");
+    loc = document.getElementById("loc");
+
+    if (navigator.geolocation) {
+        var showPosition = function(position){
+            searchByGeo(position.coords.latitude, position.coords.longitude);
+        }
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+
+function searchByGeo(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${appId}&units=${units}`)
+        .then((result) => {
+            return result.json();
+        }).then((res) => {
+            init(res);
+    });
+}
+
+function init(resultFromServer) {
+    switch (resultFromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage = "url('clearPicture.jpg')";
+            break;
+        
+        case 'Clouds':
+            document.body.style.backgroundImage = "url('cloudyPicture.jpg')";
+            break;
+
+        case 'Rain':
+        case 'Drizzle':
+            document.body.style.backgroundImage = "url('rainPicture.jpg')";
+            break;
+
+        case 'Mist':
+        case 'Haze':
+            document.body.style.backgroundImage = "url('mistPicture.jpg')";
+            break;    
+        
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = "url('stormPicture.jpg')";
+            break;
+        
+        case 'Snow':
+            document.body.style.backgroundImage = "url('snowPicture.jpg')";
+            break;
+
+        default:
+            break;
+    }
+
+    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+    let temperatureElement = document.getElementById('temperature');
+    let humidityElement = document.getElementById('humidity');
+    let windSpeedElement = document.getElementById('windSpeed');
+    let cityHeader = document.getElementById('cityHeader');
+
+    let weatherIcon = document.getElementById('documentIconImg');
+    weatherIcon.src = 'https://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
+
+    let resultDescription = resultFromServer.weather[0].description;
+    weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176;';
+    windSpeedElement.innerHTML = 'Wind Speed: ' + Math.floor(resultFromServer.wind.speed) + ' meter/s';
+    cityHeader.innerHTML = resultFromServer.name;
+    humidityElement.innerHTML = 'Humidity levels: ' + resultFromServer.main.humidity +  '%';
+
+    setPositionForWeatherInfo();
+}
+
+function setPositionForWeatherInfo() {
+    let weatherContainer = document.getElementById('weatherContainer');
+    let weatherContainerHeight = weatherContainer.clientHeight;
+    let weatherContainerWidth = weatherContainer.clientWidth;
+
+    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
+    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3}px)`;
+    weatherContainer.style.visibility = 'visible';
+}
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
+    if(searchTerm)
+        searchWeather(searchTerm);
+});
